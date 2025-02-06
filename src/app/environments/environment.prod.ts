7196df80ccf3b4815785927cd1a0cbc2a7281213
Daniel <daniromeviz11@gmail.com>
2025-02-06 14:41:22 +0100

    fix: configuración de Vercel y Angular para deployment

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     supabaseUrl: 'https://yhwlwqmcofnvfjlibvvj.supabase.co',
     supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlod2x3cW1jb2ZudmZqbGlidnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4MzMwMjgsImV4cCI6MjA1NDQwOTAyOH0.-EOMJITyUcK68-bmr-7p7hTRljX-ATikU6fg2KTTLPc'
   };
